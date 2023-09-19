82f89784e9e39503f8db4aefe6f4144a7fd4a53d
Florian Alphonzair <florian.alphonzair@etudiant.univ-rennes1.fr>
2023-09-19 13:35:58 +0200

    Init spring boot project.

---
+++ src/main/java/dao/DaoAppointment.java
@@ -0,0 +1,10 @@
+package dao;
+
+import domain.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DaoAppointment extends JpaRepository<Appointment, Long> {
+
+    public Appointment getByTeacherById(Long proID);
+
+}
